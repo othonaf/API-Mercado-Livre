59b6aeeabd939ca92a5180be8ac915c287687cba
othonaf <othon.ab@hotmail.com>
2024-01-29 16:26:19 -0300

    Atualizando Endereço no Cors

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://frontend-mercado-livre.onrender.com/"
+  origin: "*"
 }));
 
 const server = app.listen(process.env.PORT || 3003, () => {
